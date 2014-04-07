a7fcac1d99dbb2e4f5a0064cdb9979639db70c1e
oarset <oarset@gmail.com>
2014-04-07 13:39:12 +0200

    Lagt til atrubutter for Bolig klassen

--- BoligBasen/src/Bolig/Bolig.java
+++ BoligBasen/src/Bolig/Bolig.java
@@ -1,5 +1,12 @@
 package Bolig;
 
+import java.util.Date;
+
+
 public class Bolig {
 
+	private String addresse, boligtype, beskrivelse;
+	private int boa, byggeaar;
+	private int pris; // pris regnes i øre (pris = 300000 beetyr 3000 kroner)
+	private Date opprettet;
 }
