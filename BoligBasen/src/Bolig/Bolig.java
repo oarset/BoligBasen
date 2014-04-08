b62d94a6fac5a3c0af0cd659290dbefe99d2fac9
oarset <oarset@gmail.com>
2014-04-08 13:33:13 +0200

    Lagt til get og set boa/byggeår

--- BoligBasen/src/Bolig/Bolig.java
+++ BoligBasen/src/Bolig/Bolig.java
@@ -9,4 +9,28 @@ public class Bolig {
 	private int boa, byggeaar;
 	private int pris; // pris regnes i øre (pris = 300000 beetyr 3000 kroner)
 	private Date opprettet;
+	
+	// constructor oppretter bolig med adresse og type
+	public Bolig(String add, String type){
+		
+		addresse = add;
+		boligtype = type;
+		
+	}
+	// setter inn areal
+	public void setBOA(int b){
+		boa = b;
+	}
+	// henter ut boarealet
+	public int getBOA(){
+		return boa;
+	}
+	// setter inn byggeåret for boligen
+	public void setByggeAar(int aar){
+		byggeaar = aar;
+	}
+	// henter ut byggeåret for boligen
+	public int getByggeAar(){
+		return byggeaar;
+}
 }
