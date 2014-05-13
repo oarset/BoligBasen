46a15b075733d37c099fc259513daf4dd97e9f66
oarset <oarset@gmail.com>
2014-05-13 14:43:40 +0200

    Lagt til meny elementer (uten actionlisteners)

--- BoligBasen/src/GUI/BoligBaseFrame.java
+++ BoligBasen/src/GUI/BoligBaseFrame.java
@@ -7,6 +7,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JTextField;
 
 public class BoligBaseFrame extends JFrame{
@@ -21,11 +22,42 @@ public class BoligBaseFrame extends JFrame{
 		JMenu filemenu = new JMenu("Fil");
 		filemenu.setMnemonic('F');
 		
+		JMenu infomenu = new JMenu("Info");
+		infomenu.setMnemonic('I');
+		
+		// Oppretter Ny bruker valg i fil-menyen
+		JMenuItem nyBruker = new JMenuItem("Ny bruker");
+		nyBruker.setMnemonic('u');
+		
+		// Oppretter til Ny bolig valg i fil-menyen
+		JMenuItem nyBolig = new JMenuItem("Ny bolig");
+		nyBolig.setMnemonic('b');
+		
+		// Oppretter til Ny bolig valg i fil-menyen
+		JMenuItem veiviser = new JMenuItem("Veiviser");
+		veiviser.setMnemonic('b');
+				
+		// Oppretter til Ny bolig// Oppretter til Ny bolig valg i fil-menyen
+		JMenuItem om = new JMenuItem("Om...");
+		om.setMnemonic('b');
+
+		
 		// plaserer menylinjen i framen
 		JMenuBar menu = new JMenuBar();
 		setJMenuBar(menu);
-		menu.add(filemenu);
 		
+		//legger til menyer
+		menu.add(filemenu);
+		menu.add(infomenu);
+		
+		
+		//legger til inhold i fil menyen
+		filemenu.add(nyBruker);
+		filemenu.add(nyBolig);
+		
+		// legger til innhold i Info menyen
+		infomenu.add(veiviser);
+		infomenu.add(om);
 		
 		
 		header = new JTextField( 18 );
