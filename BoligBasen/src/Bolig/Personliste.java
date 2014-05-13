bb7ae65d0588e9878e964f3a35f756d8f4c5a4f1
Flammekastar <maaby200@msn.com>
2014-05-13 21:34:26 +0200

    Personliste metoder laget.

--- BoligBasen/src/Bolig/Personliste.java
+++ BoligBasen/src/Bolig/Personliste.java
@@ -1,3 +1,9 @@
+/**
+ * Personliste --- Liste med personer.
+ * @author    Alexander Maaby, Øyvind Årset
+ * Siste forandring: 13. mai
+ */
+
 package Bolig;
 
 public class Personliste {
@@ -9,15 +15,44 @@ public class Personliste {
 	}
 	
 	public void settInnPerson(Person ny) {
-		
+		if (ny == null) {
+			return;
+		}
+		if (first == null) {
+			first = ny;
+			return;
+		}
+		else {
+			Person cycle = first;
+			while (cycle.neste != null) 
+				cycle = cycle.neste;
+			cycle.neste = ny;
+		}
 	}
 	
 	public void slettPerson(Person ny) {
-		
+		if (ny == null) {
+			return;
+		}
+		if (first == ny) {
+			first = first.neste;
+			return;
+		}
+		else {
+			Person cycle = first;
+			while (cycle.neste != ny && cycle.neste != null)
+				cycle = cycle.neste;
+			if (cycle.neste == ny) {
+				cycle.neste = cycle.neste.neste;
+				return;
+			}
+			else
+				return;
+		}
 	}
 	
 	public Person finnPerson(String epost) {
-		
+		return null;
 	}
 	
 	public String toString() {
