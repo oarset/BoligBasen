af7be39c1676717cab41a37d60f935336915c6e7
oarset <oarset@gmail.com>
2014-05-14 17:17:04 +0200

    la til info om firma i Utleiere klassen

--- BoligBasen/src/Bolig/Utleiere.java
+++ BoligBasen/src/Bolig/Utleiere.java
@@ -8,7 +8,7 @@ package Bolig;
 
 public class Utleiere extends Bruker {
 	
-	private String firma;
+	private String firma, firmaInfo;
 	
 	public Utleiere(String nvn, String enavn, String adr, String pnr, String sted, int d, int m, int aa, String epost, String tlf) {
 		super(nvn, enavn, adr, pnr, sted, d, m, aa, epost, tlf);
@@ -24,6 +24,16 @@ public class Utleiere extends Bruker {
 	public String getFirma() {
 		return firma;
 	}
+	
+	//Metode for å sette informasjon om firma til en utleier.
+		public void setFirmaInfo(String selskapInfo) {
+			firmaInfo = selskapInfo;
+		}
+		
+		//Metode for å returnere en String med informasjon om firmaet til en utleier.
+		public String getFirmaInfo() {
+			return firmaInfo;
+		}
 	
 	//ToString for å hente informasjon om en utleier.
 	public String toString() {
