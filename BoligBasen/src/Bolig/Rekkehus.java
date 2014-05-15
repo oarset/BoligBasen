405b43c72af78a8843dd588cf0d2cdae6719f470
Flammekastar <maaby200@msn.com>
2014-05-15 13:00:16 +0200

    Lagt til flere typer boliger. begynt på en matchings metode.

--- BoligBasen/src/Bolig/Rekkehus.java
+++ BoligBasen/src/Bolig/Rekkehus.java
@@ -7,10 +7,34 @@
 package Bolig;
 
 public class Rekkehus extends Bolig{
-
+	
+	private int antEtg, tomt;
+	private boolean kjeller;
+	
 	public Rekkehus(String add, String type) {
 		super(add, type);
-		// TODO Auto-generated constructor stub
 	}
 
+	public void setAntEtg(int ant) {
+		antEtg = ant;
+	}
+	
+	public int getAntEtg() {
+		return antEtg;
+	}
+	
+	public void setTomt(int str) {
+		tomt = str;
+	}
+	
+	public int getTomt() {
+		return tomt;
+	}
+	
+	public void setKjeller(boolean kjlr){
+		kjeller = kjlr;
+	}
+	public boolean getKjeller() {
+		return kjeller;
+	}
 }
