acdf2f21667add53d8b296da62b88d4562531db2
Flammekastar <maaby200@msn.com>
2014-05-15 23:38:01 +0200

    Leiekontrakter toString

--- BoligBasen/src/Bolig/Leiekontrakter.java
+++ BoligBasen/src/Bolig/Leiekontrakter.java
@@ -16,4 +16,21 @@ public class Leiekontrakter implements Serializable {
 		pris = leie;
 		varighet = lengde;
 	}
+	
+	public String toString() {
+		String ut = "";
+		if (utleieboligen.getAdresse() != null) 
+			ut += "Adresse: " + utleieboligen.getAdresse() + "\n";
+		if (utleier != null) {
+			ut += "Utleier brukerid: " + utleier.getBrukerid() + "\n";
+			ut += "Utleier navn: " + utleier.nametoString()  + "\n";
+			}
+		if (leietaker != null) {
+			ut += "Leietaker brukerid: " + leietaker.getBrukerid()  + "\n";
+			ut += "Leietaker navn: " + leietaker.nametoString()  + "\n";
+		}
+		ut += "Leiepris: " + pris + "\n";
+		ut += "Varighet på låneavtale: " + varighet  + "\n";
+		return ut;
+	}
 }
